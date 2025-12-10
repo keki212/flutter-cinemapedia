6da12bb589e8279d3fb19784aedd2401981bee42
Heduin Lopez <eduinlopezc@gmail.com>
2025-12-10 10:58:48 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# cinemapedia
+
+# Dev
+
+1. Copiar .env.example a .env y configurar las variables de entorno
+2. Cambiar las variables de entorno.
+
---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  ThemeData getTheme() =>
+      ThemeData(
+        colorSchemeSeed: const Color(0xff2822F5),
+        useMaterial3: true,
+      );
+}
