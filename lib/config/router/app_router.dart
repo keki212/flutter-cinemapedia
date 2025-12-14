a6c2792a7dd752c8e7b052d4157c508c2e80d0fd
Heduin Lopez <eduinlopezc@gmail.com>
2025-12-14 11:32:04 -0500

    End custom go_router

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -3,12 +3,15 @@ import 'package:go_router/go_router.dart';
 import 'package:cinemapedia/presentation/screens/screens.dart';
 
 final appRouter = GoRouter(
-  initialLocation: '/',
+  initialLocation: '/home/0',
   routes: [
     GoRoute(
-      path: '/',
+      path: '/home/:page',
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen(),
+      builder: (context, state){
+        final pageIndex = int.parse(state.pathParameters['page'] ?? '0');
+        return HomeScreen(pageIndex: pageIndex);
+      },
       routes: [
         GoRoute(
           path: 'movie/:id',
@@ -20,5 +23,9 @@ final appRouter = GoRouter(
         ),
       ],
     ),
+    GoRoute(
+      path: '/',
+      redirect: (_, _) => '/home/0',
+      )
   ],
 );
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -47,7 +47,7 @@ class CustomAppbar extends ConsumerWidget {
 
                   if (movie == null) return;
                   if (context.mounted) {
-                    context.push('/movie/${movie.id}');
+                    context.push('/home/0/movie/${movie.id}');
                   }
                 },
                 icon: Icon(Icons.search, color: colors.primary),
